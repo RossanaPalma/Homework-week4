b1a21adba8ea407cf9fbae1a216e9f939e40022c
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2021-09-20 23:21:42 -0700

    Add project files.

---
+++ Project 2/Program.cs
@@ -0,0 +1,49 @@
+﻿/// Chapter No. 9 and 10 - Homework_week4
+/// File Name: homework_week4.java
+/// @author: Rossana Palma
+/// Date:  September 19, 2021
+///
+/// Problem Statement: Write a program that determines the change to be dispensed from a vending machine.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Declare variables
+/// 2) Prompt user for price and convert text to int
+/// 3) Calculate change to be dispense from a vending machine
+/// 4) Display results on the screen
+
+
+using System;
+
+namespace Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+            int item1, item2, item3, item4, amount;
+
+            //Prompt user for price
+            Console.WriteLine("Enter price of item (from 25 cents to a dollar, in 5-cent increments):");
+            String inputPrice;
+            inputPrice = Console.ReadLine();
+            //Convert to int
+            item1 = Int32.Parse(inputPrice);
+
+            // Calculations
+            amount = 100 - item1;
+            item2 = amount / 25;
+            amount = amount % 25;
+            item3 = amount / 10;
+            item4 = amount / 5;
+
+            // Display
+
+            Console.WriteLine("You bought an item for " + item1 + " cents and gave me a dollar, so your change is: ");
+            Console.WriteLine(item2 + " quarters");
+            Console.WriteLine(item3 + " dimes");
+            Console.WriteLine(item4 + " nickles");
+        }
+    }
+}
---
+++ Project1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿/// Chapter No. 9 and 10 - Homework_Week4
+/// File Name: homework_week4.java
+/// @author: Rossana Palma
+/// Date:  September 19, 2021
+///
+/// Problem Statement: Write a GUI program that calculates monthly principal, monthly interest, monthly 
+/// mortgage and the outstandings balance. Assume that the annual interest is 6.39 percent.
+/// 
+/// Overall Plan:
+/// 1) Create text boxs, labels and buttom in GUI application
+/// 2) Declare variables in console application
+/// 3) Convert mortgageAmount and annualInterest to double
+/// 4) Calculate monthlyInterest, monthlyPrincipal, monthlyMortgage and outstandingBalance
+/// 5) Display results on the screen
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Declare variables
+            double mortgageAmount, annualInterest, monthlyMortgage, monthlyInterest, monthlyPrincipal, outstandingBalance;
+
+            //Convert to double
+            mortgageAmount = Convert.ToDouble(mortgageAmountTextBox.Text);
+            annualInterest = Convert.ToDouble(annualInterestTextBox.Text);
+
+            //Calculation
+            monthlyInterest = mortgageAmount * ((annualInterest/100)/12);
+            monthlyPrincipal = mortgageAmount / 12;
+            monthlyMortgage = monthlyPrincipal + monthlyInterest;
+            outstandingBalance = mortgageAmount - monthlyMortgage;
+            
+            // Display 
+            mortgageAmountTextBox.Text = mortgageAmount.ToString("c");
+            outstandingTextBox.Text = outstandingBalance.ToString("c");
+            annualInterestTextBox.Text = annualInterest.ToString("");
+            monthlyMortgageTextBox.Text = monthlyMortgage.ToString("c");
+            interestAmountTextBox.Text = monthlyInterest.ToString("c");
+            principalAmountTextBox.Text = monthlyPrincipal.ToString("c");
+            
+        }
+    }
+    }
+
